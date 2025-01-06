78140b36ee996b91949405c3dabbdcdcc650bc9c
Kevin <125kaguilarmartinez@frhsd.com>
2025-01-06 13:52:41 -0500

    awmdklmwalkd

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ const scheduleData = [
       "class": "AP Environmental Science",
       "room": "B211",
       "teacher": "McQuade, Tara",
-      "days": ["A", "E"]
+      "days": ["A",]
   },
   {
       "period": 2,
-      "startTime": "7:30 a.m.",
-      "endTime": "8:37 a.m.",
+      "startTime": "8:42 a.m.",
+      "endTime": "9:49 a.m.",
       "class": "AP Environmental Science",
       "room": "B211",
       "teacher": "McQuade, Tara",
-      "days": ["A", "B"]
+      "days": ["A",]
   },
   {
       "period": 3,
@@ -24,7 +24,7 @@ const scheduleData = [
       "class": "Calculus",
       "room": "B202",
       "teacher": "Blum, Jason",
-      "days": ["A", "C", "D", "E", "G"]
+      "days": ["A",]
   },
   {
       "period": 4,
@@ -33,35 +33,367 @@ const scheduleData = [
       "class": "Lunch",
       "room": "Lunch",
       "teacher": "Lunch",
-      "days": ["B", "C", "D", "F", "G"]
+      "days": ["A",]
   },
   {
       "period": 5,
-      "startTime": "11:06 a.m.",
-      "endTime": "11:41 a.m.",
-      "class": "Lunch",
-      "room": "Lunch",
-      "teacher": "Lunch",
-      "days": ["A", "B", "D", "E", "G"]
-  },
-  {
-      "period": 6,
       "startTime": "11:47 a.m.",
       "endTime": "12:54 a.m.",
       "class": "Fine Art 1",
       "room": "B120",
       "teacher": "Finaldi, Daniel",
-      "days": ["A", "B", "D", "E", "F"]
+      "days": ["A",]
   },
   {
-      "period": 7,
+      "period": 6,
       "startTime": "12:59 a.m.",
       "endTime": "2:06 a.m.",
       "class": "Honors NJIT IS219 Advanced Website Development W",
       "room": "C104",
       "teacher": "McDonald, Ashley",
-      "days": ["A", "B", "D", "E", "F"]
+      "days": ["A",]
   },
+  {
+    "period": 1,
+    "startTime": "7:30 a.m.",
+    "endTime": "8:37 a.m.",
+    "class": "AP Spanish Language",
+    "room": "A108",
+    "teacher": "Fernandez-Cohen, Camila",
+    "days": ["B",]
+},
+
+{
+    "period": 2,
+    "startTime": "8:42 a.m.",
+    "endTime": "9:49 a.m.",
+    "class": "AP Environmental Science",
+    "room": "B211",
+    "teacher": "McQuade, Tara",
+    "days": ["B",]
+},
+
+{
+    "period": 3,
+    "startTime": "9:54 a.m.",
+    "endTime": "11:01 a.m.",
+    "class": "Health 12 Lab",
+    "room": "GYM1",
+    "teacher": "Agabiti, Adrienne",
+    "days": ["B",]
+},
+{
+    "period": 4,
+    "startTime": "11:06 a.m.",
+    "endTime": "11:41 a.m.",
+    "class": "Lunch",
+    "room": "Lunch",
+    "teacher": "Lunch",
+    "days": ["B",]
+},
+{
+    "period": 5,
+    "startTime": "11:47 a.m.",
+    "endTime": "12:54 a.m.",
+    "class": "Ap English Lit & Comp",
+    "room": "A121",
+    "teacher": "Vona, Christopher",
+    "days": ["B",]
+},
+{
+    "period": 6,
+    "startTime": "12:59 a.m.",
+    "endTime": "2:06 a.m.",
+    "class": "Fine Art 1",
+    "room": "B120",
+    "teacher": "Finaldi, Daniel",
+    "days": ["B",]
+},
+
+{
+    "period": 1,
+    "startTime": "7:30 a.m.",
+    "endTime": "8:37 a.m.",
+    "class": "Calculus",
+    "room": "B202",
+    "teacher": "Blum, Jason",
+    "days": ["C",]
+},
+
+{
+    "period": 2,
+    "startTime": "8:42 a.m.",
+    "endTime": "9:49 a.m.",
+    "class": "AP Spanish Language",
+    "room": "A108",
+    "teacher": "Fernandez-Cohen, Camila",
+    "days": ["C",]
+},
+
+{
+    "period": 3,
+    "startTime": "9:54 a.m.",
+    "endTime": "11:01 a.m.",
+    "class": "AP Environmental Science",
+    "room": "B211",
+    "teacher": "McQuade, Tara",
+    "days": ["C",]
+},
+{
+    "period": 4,
+    "startTime": "11:06 a.m.",
+    "endTime": "11:41 a.m.",
+    "class": "Lunch",
+    "room": "Lunch",
+    "teacher": "Lunch",
+    "days": ["C",]
+},
+{
+    "period": 5,
+    "startTime": "11:47 a.m.",
+    "endTime": "12:54 a.m.",
+    "class": "Honors NJIT IS219 Advanced Website Development W",
+    "room": "C104",
+    "teacher": "McDonald, Ashley",
+    "days": ["C",]
+},
+{
+    "period": 6,
+    "startTime": "12:59 a.m.",
+    "endTime": "2:06 a.m.",
+    "class": "Ap English Lit & Comp",
+    "room": "A121",
+    "teacher": "Vona, Christopher",
+    "days": ["C",]
+},
+
+{
+    "period": 1,
+    "startTime": "7:30 a.m.",
+    "endTime": "8:37 a.m.",
+    "class": "Health 12 Lab",
+    "room": "GYM1",
+    "teacher": "Agabiti, Adrienne",
+    "days": ["D",]
+},
+
+{
+    "period": 2,
+    "startTime": "8:42 a.m.",
+    "endTime": "9:49 a.m.",
+    "class": "Calculus",
+    "room": "B202",
+    "teacher": "Blum, Jason",
+    "days": ["D",]
+},
+
+{
+    "period": 3,
+    "startTime": "9:54 a.m.",
+    "endTime": "11:01 a.m.",
+    "class": "AP Spanish Language",
+    "room": "A108",
+    "teacher": "Fernandez-Cohen, Camila",
+    "days": ["D",]
+},
+{
+    "period": 4,
+    "startTime": "11:06 a.m.",
+    "endTime": "11:41 a.m.",
+    "class": "Lunch",
+    "room": "Lunch",
+    "teacher": "Lunch",
+    "days": ["D",]
+},
+{
+    "period": 5,
+    "startTime": "11:47 a.m.",
+    "endTime": "12:54 a.m.",
+    "class": "Fine Art 1",
+    "room": "B120",
+    "teacher": "Finaldi, Daniel",
+    "days": ["D",]
+},
+{
+    "period": 6,
+    "startTime": "12:59 a.m.",
+    "endTime": "2:06 a.m.",
+    "class": "Honors NJIT IS219 Advanced Website Development W",
+    "room": "C104",
+    "teacher": "McDonald, Ashley",
+    "days": ["D",]
+},
+
+{
+    "period": 1,
+    "startTime": "7:30 a.m.",
+    "endTime": "8:37 a.m.",
+    "class": "AP Environmental Science",
+    "room": "B211",
+    "teacher": "McQuade, Tara",
+    "days": ["E",]
+},
+
+{
+    "period": 2,
+    "startTime": "8:42 a.m.",
+    "endTime": "9:49 a.m.",
+    "class": "Health 12 Lab",
+    "room": "GYM1",
+    "teacher": "Agabiti, Adrienne",
+    "days": ["E",]
+},
+
+{
+    "period": 3,
+    "startTime": "9:54 a.m.",
+    "endTime": "11:01 a.m.",
+    "class": "Calculus",
+    "room": "B202",
+    "teacher": "Blum, Jason",
+    "days": ["E",]
+},
+{
+    "period": 4,
+    "startTime": "11:06 a.m.",
+    "endTime": "11:41 a.m.",
+    "class": "Lunch",
+    "room": "Lunch",
+    "teacher": "Lunch",
+    "days": ["E",]
+},
+{
+    "period": 5,
+    "startTime": "11:47 a.m.",
+    "endTime": "12:54 a.m.",
+    "class": "Ap English Lit & Comp",
+    "room": "A121",
+    "teacher": "Vona, Christopher",
+    "days": ["E",]
+},
+{
+    "period": 6,
+    "startTime": "12:59 a.m.",
+    "endTime": "2:06 a.m.",
+    "class": "Fine Art 1",
+    "room": "B120",
+    "teacher": "Finaldi, Daniel",
+    "days": ["E",]
+},
+
+{
+    "period": 1,
+    "startTime": "7:30 a.m.",
+    "endTime": "8:37 a.m.",
+    "class": "AP Spanish Language",
+    "room": "A108",
+    "teacher": "Fernandez-Cohen, Camila",
+    "days": ["F",]
+},
+
+{
+    "period": 2,
+    "startTime": "8:42 a.m.",
+    "endTime": "9:49 a.m.",
+    "class": "AP Environmental Science",
+    "room": "B211",
+    "teacher": "McQuade, Tara",
+    "days": ["F",]
+},
+
+{
+    "period": 3,
+    "startTime": "9:54 a.m.",
+    "endTime": "11:01 a.m.",
+    "class": "Health 12 Lab",
+    "room": "GYM1",
+    "teacher": "Agabiti, Adrienne",
+    "days": ["F",]
+},
+{
+    "period": 4,
+    "startTime": "11:06 a.m.",
+    "endTime": "11:41 a.m.",
+    "class": "Lunch",
+    "room": "Lunch",
+    "teacher": "Lunch",
+    "days": ["F",]
+},
+{
+    "period": 5,
+    "startTime": "11:47 a.m.",
+    "endTime": "12:54 a.m.",
+    "class": "Honors NJIT IS219 Advanced Website Development W",
+    "room": "C104",
+    "teacher": "McDonald, Ashley",
+    "days": ["F",]
+},
+{
+    "period": 6,
+    "startTime": "12:59 a.m.",
+    "endTime": "2:06 a.m.",
+    "class": "Ap English Lit & Comp",
+    "room": "A121",
+    "teacher": "Vona, Christopher",
+    "days": ["F",]
+},
+
+{
+    "period": 1,
+    "startTime": "7:30 a.m.",
+    "endTime": "8:37 a.m.",
+    "class": "Calculus",
+    "room": "B202",
+    "teacher": "Blum, Jason",
+    "days": ["G",]
+},
+
+{
+    "period": 2,
+    "startTime": "8:42 a.m.",
+    "endTime": "9:49 a.m.",
+    "class": "AP Spanish Language",
+    "room": "A108",
+    "teacher": "Fernandez-Cohen, Camila",
+    "days": ["G",]
+},
+
+{
+    "period": 3,
+    "startTime": "9:54 a.m.",
+    "endTime": "11:01 a.m.",
+    "class": "Ap English Lit & Comp",
+    "room": "A121",
+    "teacher": "Vona, Christopher",
+    "days": ["G",]
+},
+{
+    "period": 4,
+    "startTime": "11:06 a.m.",
+    "endTime": "11:41 a.m.",
+    "class": "Lunch",
+    "room": "Lunch",
+    "teacher": "Lunch",
+    "days": ["G",]
+},
+{
+    "period": 5,
+    "startTime": "11:47 a.m.",
+    "endTime": "12:54 a.m.",
+    "class": "Fine Art 1",
+    "room": "B120",
+    "teacher": "Finaldi, Daniel",
+    "days": ["G",]
+},
+{
+    "period": 6,
+    "startTime": "12:59 a.m.",
+    "endTime": "2:06 a.m.",
+    "class": "Honors NJIT IS219 Advanced Website Development W",
+    "room": "C104",
+    "teacher": "McDonald, Ashley",
+    "days": ["G",]
+},
 ];
 
 $(document).ready(function () {
